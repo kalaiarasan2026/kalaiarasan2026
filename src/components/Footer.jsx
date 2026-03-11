8d39e2928f07b4021b8852a6abf18693430e2b39
Kalai Arasan <145125390+kalaiarasan2026@users.noreply.github.com>
2026-03-11 17:30:43 +0900

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      © 2026 Kalai Arasan • Built with React & TailwindCSS
+    </footer>
+  );
+}
