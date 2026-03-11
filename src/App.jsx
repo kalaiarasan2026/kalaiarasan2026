abdedb6cfb5ed5d2d197f916833311240a87a8e7
Kalai Arasan <145125390+kalaiarasan2026@users.noreply.github.com>
2026-03-11 17:24:45 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+@import Navbar from "./components/Navbar";
+@import Home from "./components/Home";
+@import About from "./components/About";
+@import Skills from "./components/Skills";
+@import Projects from "./components/Projects";
+@import Contact from "./components/Contact";
 @import Footer from "./components/Footer";
 
 function App() {
